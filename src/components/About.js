074f655b403cfd24bae115c6058a9ec1c7d51d5a
x-russian <philophilo037@gmail.com>
2023-10-14 19:02:51 +0300

    :sparkles: Access and use the props in about and home componenents.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function About() {
+function About({props}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+
+      <h1>Links</h1>
+      <a href="f">
+      <p>{props.links.github}</p>
+      <p>{props.links.linkedin}</p>
+      </a>
+      
       {/* add your <Links /> component here */}
     </div>
   );
